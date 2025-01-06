70b079b5f056aa0b7a3d683afaf6e8b9fbebc9bc
surfde22 <surfde22@gmail.com>
2025-01-06 12:32:04 -0600

    Learning to read and write CSV and JSON files in Python

---
+++ Chapter3/readcsv.py
@@ -0,0 +1,7 @@
+import csv
+
+with open('/home/paulcrickard/data.csv') as f:
+	myreader=csv.DictReader(f)
+	headers=next(myreader)
+	for row in myreader:
+		print(row['name'])
---
+++ Chapter3/readjson.py
@@ -0,0 +1,6 @@
+import json
+
+with open("data.json","r") as f:
+	data=json.load(f)
+print(type(data))
+print(data['records'][0]['name'])
