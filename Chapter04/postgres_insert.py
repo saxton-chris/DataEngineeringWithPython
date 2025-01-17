d487c254df2fbd38306ca8dfd97138f7fa70b152
surfde22 <surfde22@gmail.com>
2025-01-17 11:29:14 -0600

    updating comments

--- Chapter04/postgres_insert.py
+++ Chapter04/postgres_insert.py
@@ -1,11 +1,12 @@
 import psycopg2 as db
 from faker import Faker
 
+# Initialize the Faker library for generating fake data
 fake = Faker()
 data = []
 i = 2
 
-# generate data to load into Postgres DB
+# Generate 1000 rows of fake data to load into a PostgreSQL database
 for r in range(1000):
     data.append((i, fake.name(), fake.street_address(), fake.city(), fake.zipcode()))
     i += 1
@@ -15,17 +16,23 @@ data_for_db = tuple(data)
 #print(data)
 #print(data_for_db)
 
-# Connecting to the DB
+# Connection string to connect to the PostgreSQL database
 conn_string = "dbname = 'dataengineering' host = 'localhost' user = 'postgres' password = '*****'"
+
+# Establish a connection to the database
 conn = db.connect(conn_string)
 cur = conn.cursor()
 
 #print(conn)
 
-# Build and check the query
+# Build and check the SQL query for inserting data
 query = "INSERT INTO users (id, name, street, city, zip) VALUES (%s, %s, %s, %s, %s)"
-print(cur.mogrify(query, data_for_db[1]))
+print(cur.mogrify(query, data_for_db[1])) # Print a sample query to verify correctness
 
-# Insert into Postgres DB - users table
+# Execute the query with the generated data and commit the transaction
 cur.executemany(query, data_for_db)
 conn.commit()
+
+# Close the cursor and connection
+cur.close()
+conn.close()
