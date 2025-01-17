b4521b49f1677ee7f2eee514be4ad47111b116ae
surfde22 <surfde22@gmail.com>
2025-01-17 11:53:50 -0600

    Optimzed using ChatGPT

--- Chapter04/dataframes_extract.py
+++ Chapter04/dataframes_extract.py
@@ -4,11 +4,10 @@ import pandas as pd
 # Define the connection string to connect to the PostgreSQL database
 conn_string = "dbname = 'dataengineering' host = 'localhost' user = 'postgres' password = 'ollie'"
 
-# Establish a connection to the database
-conn = db.connect(conn_string)
+# Establish a connection to the database and fetch data into a DataFrame
+with db.connect(conn_string) as conn:
+    # Use pandas to execute a SQL query and fetch the data into a DataFrame
+    df = pd.read_sql_query("SELECT * FROM users", conn)
 
-# Use pandas to execute a SQL query and fetch the data into a DataFrame
-df = pd.read_sql("SELECT * FROM users", conn)
-
-# Save the DataFrame as a JSON file in records orientation
-df.to_json("../output/data_ch04.json", orient='records')
+    # Save the DataFrame as a JSON file in records orientation
+    df.to_json("../output/data_ch04.json", orient='records', indent=4)
