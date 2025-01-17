38945495b1825514b8d1ecb21308b7876fb91234
surfde22 <surfde22@gmail.com>
2025-01-17 11:43:43 -0600

    optimized with ChatGPT

--- Chapter04/postgres_extract.py
+++ Chapter04/postgres_extract.py
@@ -1,26 +1,23 @@
 import psycopg2 as db
 
 # Define the connection string to connect to the PostgreSQL database
-conn_string = conn_string = "dbname = 'dataengineering' host = 'localhost' user = 'postgres' password = 'ollie'"
+conn_string = "dbname = 'dataengineering' host = 'localhost' user = 'postgres' password = 'ollie'"
 
 # Establish a connection to the database
-conn = db.connect(conn_string)
-cur = conn.cursor()
+with db.connect(conn_string) as conn:
+    with conn.cursor() as cur:
+        # Query to select all data from the 'users' table
+        query = "SELECT * FROM users"
+        cur.execute(query)
 
-# Query to select all data from the 'users' table
-query = "SELECT * FROM users"
-cur.execute(query)
+        # Fetch all records and print them
+        records = cur.fetchall()
+        for record in records:
+            print(record)
 
-# Print each record retrieved by the query
-for record in cur:
-    print(record)
+        # Print the total number of rows fetched by the query
+        print(len(records))
 
-# Print the total number of rows fetched by the query
-print(cur.rowcount)
-
-# Open a file to write the data
-f = open("../output/data_ch04.csv", "w")
-
-# Use copy_to to write the 'users' table data to a CSV file
-cur.copy_to(f, 'users', sep=',')
-f.close()
+        # Write the data to a CSV file
+        with open("../output/data_ch04.csv", "w") as f:
+            cur.copy_to(f, 'users', sep=',')
